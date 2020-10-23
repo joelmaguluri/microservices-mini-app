b2cbc094b4a8dc494f289c00eea9e6bfd9407c4f
sudeep987 <msudeep.joel@gmail.com>
2020-10-23 23:34:59 +0530

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express =require('express')
+const bodyParser=require('body-parser')
+const {randomBytes}=require('crypto')
+const cors=require('cors')
+const app =express()
+app.use(cors());
+var posts={
+
+}
+
+app.use(bodyParser.json({}))
+
+app.get('/posts',(req,res)=>{
+  res.send(posts);
+})
+
+app.post('/posts',(req,res)=>{
+  const id=randomBytes(4).toString('hex');
+  console.log(req.body);
+  const {title}=req.body;
+  posts[id]={
+    id,title
+  }
+  console.log(req.body)
+  res.status(201).send(posts[id]);
+})
+
+app.listen(4000,()=>{
+  console.log('started at port 4000');
+})
